5bed5c28cca83a1aae1e022fb9fcce5ae444a809
Asadbek001011 <egamnazarovasadbek50@gmail.com>
2024-10-28 23:09:19 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let user = prompt('Ismingiz');
+alert(user);
+
+let age = prompt('Familyangiz');
+alert(age);
+
+let Yosh = prompt('Yoshingiz');
+alert(Yosh);
+
+let YashashJoyingiz = prompt('Yashash Joyingiz');
+alert(YashashJoyingiz);
+
+let YoqtirganFaningiz = prompt('Yoqtirgan Faningiz','Informatika');
+alert(YoqtirganFaningiz);
+
+let ageName = ('Malumotlaringizni kiritganingiz uchun raxmat');
+alert(ageName);
